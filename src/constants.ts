6d817ced44722e3d7eeb3eb719d5e6e6792b6eca
elma <elma.sarajlic@stu.ibu.edu.ba>
2024-09-19 15:47:20 +0200

    deploy

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
 
 
-
-  export const BASE_URL = 'https://diplomskibackend-1.onrender.com'
+  export const BASE_URL = 'https://diplomskibackend-1.onrender.com/api'
   export const StripePublicKey = "pk_test_51PLXlXP9f58hdp8j5Tq1WxaKUKtjG2SUdf0TiifZMeZwSrh3GDVH50nIVQvGe90tQunzGS2YCzFxn0tfJQAtRATV00ziArJncA"
